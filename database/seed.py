c66975611d256a5e7c36d7286348485873e63ca1
djudjuu <julfaber@gmail.com>
2021-08-18 14:12:53 +0200

    log seeding

--- database/seed.py
+++ database/seed.py
@@ -9,11 +9,16 @@ import os
 from database.test.conftest import reset_db
 from database.db import SessionLocal
 from database import crud
+from utils.logger import  get_logger
+
+logger = get_logger(__name__)
+
+logger.info('Applying Seed Data')
 
 db_session = SessionLocal()
 
 load_dotenv()
-reset_db(deleteWhitelist=True)
+reset_db(db_session)
 GURUGRUPA_CUSTOMER_ID = os.getenv("GURUGRUPA_CUSTOMER_ID")
 GURUGRUPA_RECEIVERS = [
  PurchaserInfo(id='dd06ff1a-d9f2-4b2a-8182-d9d2e9522d8e', name='SHri verabhadreshwar medical store', phone='+91-9448186108', city='Laxmeshwar', location_id='030a0c98-d7e2-473c-afba-bed41feb2960'),
@@ -102,3 +107,4 @@ for purchaser in GURUGRUPA_RECEIVERS[-2:]:
 	)
 
 
+logger.info('Applying Seed Data Completed')
