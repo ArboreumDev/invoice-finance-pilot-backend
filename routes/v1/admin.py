5f1a9094c7c67d051d8916fc4bd5396a89e61b2b
djudjuu <julfaber@gmail.com>
2021-08-24 11:57:08 +0200

    simplified endpoint: all args in body

--- routes/v1/admin.py
+++ routes/v1/admin.py
@@ -1,6 +1,6 @@
-from typing import Tuple
+from typing import Tuple, Optional
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Body
 from sqlalchemy.orm import Session
 from starlette.status import HTTP_400_BAD_REQUEST, HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 
@@ -10,36 +10,40 @@ from database.exceptions import UnknownPurchaserException
 from invoice.tusker_client import tusker_client
 from routes.dependencies import get_db
 from utils.security import check_jwt_token_role
+from utils.common import CamelModel
 
 # ===================== routes ==========================
 admin_app = APIRouter()
 
 # DESC: this is just a very fast hack to have an admin interface so that non-technical
 # people can play around with it without having to use dev-tools like insomnia
+class InvoiceUpdateInput(CamelModel):
+    invoice_id: str
+    new_status: Optional[str]
+    new_value: Optional[str]
+    loan_id: Optional[str]
+    tx_id: Optional[str]
 
 
-@admin_app.post("/update/value/{invoiceId}/{value}")
-def update_invoice_value(invoiceId: str, value: int, db: Session = Depends(get_db), user_info: Tuple[str, str] = Depends(check_jwt_token_role)):
-    _, role = user_info
-    if role != "loanAdmin":
-        raise HTTPException(HTTP_401_UNAUTHORIZED, "Missing admin credentials")
-    invoice_service.update_invoice_value(invoiceId, int(value), db)
-    return {"OK"}
-
-
-@admin_app.post("/update/status/{invoiceId}/{new_status}")
+@admin_app.post("/update")
 def update_invoice_finance_status(
-        invoiceId: str, new_status: str, loan_id: str = "", tx_id: str = "",
+        update: InvoiceUpdateInput = Body(..., embed=True),
         db: Session = Depends(get_db),
         user_info: Tuple[str, str] = Depends(check_jwt_token_role)
     ):
     _, role = user_info
     if role != "loanAdmin":
         raise HTTPException(HTTP_401_UNAUTHORIZED, "Missing admin credentials")
-    if new_status == "FINANCED" and not loan_id:
-        raise HTTPException(HTTP_400_BAD_REQUEST, "Missing value for loan_id")
-    invoice_service.update_invoice_payment_status(
-        db=db, invoice_id=invoiceId, new_status=new_status, loan_id=loan_id, tx_id=tx_id
-    )
+
+    print('in', update)
+    if update.new_value:
+        invoice_service.update_invoice_value(update.invoice_id, update.new_value, db)
+
+    if update.new_status:
+        if update.new_status == "FINANCED" and not update.loan_id:
+            raise HTTPException(HTTP_400_BAD_REQUEST, "Missing value for loan_id")
+        invoice_service.update_invoice_payment_status(
+            db=db, invoice_id=update.invoice_id, new_status=update.new_status, loan_id=update.loan_id, tx_id=update.tx_id
+        )
     return {"OK"}
 
