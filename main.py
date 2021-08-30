bc542b244f62276af7fc63f928eb0ad04ce72a40
djudjuu <julfaber@gmail.com>
2021-08-30 10:53:50 +0200

    login logger to separate file

--- main.py
+++ main.py
@@ -43,11 +43,10 @@ def read_root():
 
 @app.post("/token", description=TOKEN_DESCRIPTION, summary="JWT Auth", tags=["auth"])
 def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    logger = get_logger(__name__)
+    logger = get_logger('login')
     """ create a token with role set to whatever is in our database """
     jwt_user_dict = {"username": form_data.username, "password": form_data.password}
     jwt_user = JWTUser(**jwt_user_dict)
-    print('got', jwt_user)
     logger.info(f"login attempt by user {form_data.username}")
 
     role = authenticate_user(db, jwt_user)
