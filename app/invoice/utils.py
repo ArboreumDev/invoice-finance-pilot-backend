ef9f3d78a6b8b14da0cef3a3109dfe77e3add06f
djudjuu <julfaber@gmail.com>
2021-12-22 00:02:13 +0100

    lint

--- app/invoice/utils.py
+++ app/invoice/utils.py
@@ -85,6 +85,7 @@ def db_invoice_to_frontend_info(inv: Invoice, purchaser: Whitelist, supplier: Su
         ),
     )
 
+
 def invoice_to_principal(inv: Invoice):
     payment_details = json.loads(inv.payment_details)
-    return payment_details['principal']
+    return payment_details["principal"]
